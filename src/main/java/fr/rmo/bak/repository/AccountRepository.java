9ba7234ee8efc380cec34aed7c2dfbe6dc4275f1
rmo <a@b.com>
2017-12-12 22:54:14 +0100

    Account Repository implementation

--- src/main/java/fr/rmo/bak/repository/AccountRepository.java
+++ src/main/java/fr/rmo/bak/repository/AccountRepository.java
@@ -1,5 +1,7 @@
 package fr.rmo.bak.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import fr.rmo.bak.domain.Account;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
+	Optional<Account> findByNumber(String number);
 }
--- src/test/java/fr/rmo/bak/repository/AccountRepositoryTest.java
+++ src/test/java/fr/rmo/bak/repository/AccountRepositoryTest.java
@@ -1,11 +1,57 @@
 package fr.rmo.bak.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import fr.rmo.bak.domain.Account;
+
+/**
+ * Test class for Account repository
+ */
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class AccountRepositoryTest {
 
+	private static final String ACCOUNT_NUMBER_TEST = "ACC-007";
+
+	@Autowired
+	private TestEntityManager testEntityManager;
+
+	@Autowired
+	private AccountRepository accountRepository;
+
+	@Before
+	public void setUp() {
+		Account account = new Account();
+		account.setNumber(ACCOUNT_NUMBER_TEST);
+		account.setBalance(0L);
+
+		testEntityManager.persist(account);
+	}
+
+	@Test
+	public void whenFindByAccountNumberThenOk() {
+		Optional<Account> account = accountRepository.findByNumber(ACCOUNT_NUMBER_TEST);
+
+		assertThat(account).isNotNull();
+		assertThat(account.isPresent()).isTrue();
+		assertThat(account.get().getNumber()).isEqualTo(ACCOUNT_NUMBER_TEST);
+	}
+
+	@Test
+	public void whenFindByAccountNumberThenNotFound() {
+		Optional<Account> account = accountRepository.findByNumber("DUMMY");
+
+		assertThat(account).isNotNull();
+		assertThat(account.isPresent()).isFalse();
+	}
 }
